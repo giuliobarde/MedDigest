c044189213fe42ddc08f1a92559c10e9ee712e4b
giuliobarde <giuliobarde@gmail.com>
2025-06-04 13:23:07 +0200

    papers dictionary commit

--- fetch_medical_week.py
+++ fetch_medical_week.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+import xml.etree.ElementTree as ET
 
 now_utc = datetime.datetime.now(datetime.timezone.utc)
 
@@ -35,5 +36,28 @@ url = (
 )
 
 response = requests.get(url)
+root = ET.fromstring(response.content)
 
-print(response.text)
+# Define the namespace
+ns = {'atom': 'http://www.w3.org/2005/Atom'}
+
+# Initialize the list to store papers
+papers_this_week = []
+
+# Parse each entry in the response
+for entry in root.findall('.//atom:entry', ns):
+    paper = {
+        "paper_id": entry.find('atom:id', ns).text.split('/')[-1],
+        "title": entry.find('atom:title', ns).text,
+        "link": entry.find('atom:link', ns).get('href'),
+        "published_date": datetime.datetime.strptime(
+            entry.find('atom:published', ns).text,
+            '%Y-%m-%dT%H:%M:%SZ'
+        ).replace(tzinfo=datetime.timezone.utc),
+        "abstract_text": entry.find('atom:summary', ns).text.strip()
+    }
+    papers_this_week.append(paper)
+
+mini_summaries = []
+for paper in papers_this_week:
+    print(paper["paper_id"])
